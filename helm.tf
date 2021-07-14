7fde3e149265d6ed1e5e26134302b214d49d33b8
Martin Ansong <martin.ansong@protonmail.com>
2021-07-14 12:30:02 +0100

    fix format

--- helm.tf
+++ helm.tf
@@ -1,7 +1,7 @@
 provider "helm" {
   kubernetes {
-    host                   = google_container_cluster.primary.endpoint
-  
+    host = google_container_cluster.primary.endpoint
+
     client_certificate     = google_container_cluster.primary.master_auth.0.client_certificate
     client_key             = google_container_cluster.primary.master_auth.0.client_key
     cluster_ca_certificate = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
